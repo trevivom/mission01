4afcc7bd9caa448a67290bcb8fa7f985ea8d5de2
sungwon <trevivom76@naver.com>
2023-07-03 18:47:10 +0900

    file commit.

---
+++ src/main/java/com/example/mission01/domain/History.java
@@ -0,0 +1,46 @@
+package com.example.mission01.domain;
+
+import java.sql.Date;
+
+public class History {
+
+    private int histNo;
+
+    private String longitude;
+
+    private String latitude;
+
+    private Date searchDate;
+
+    public int getHistNo() {
+        return histNo;
+    }
+
+    public void setHistNo(int histNo) {
+        this.histNo = histNo;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public Date getSearchDate() {
+        return searchDate;
+    }
+
+    public void setSearchDate(Date searchDate) {
+        this.searchDate = searchDate;
+    }
+}
